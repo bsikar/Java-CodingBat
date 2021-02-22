1cb10d921b4e8b9891f078e3b878330e5fbf391a
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-02-21 22:36:57 -0600

    Revert "Revert "added licence""
    This reverts commit 66d12c2777dd55a512561ed857604a867821d236.

---
+++ String-1/withoutX2.java
@@ -0,0 +1,44 @@
+/* MIT License
+ *
+ * Copyright (c) 2021 Brighton Sikarskie
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+public String withoutX2(String str) {
+  switch (str.length()) {
+  case 0: 
+    return "";
+  case 1:
+    if (str.charAt(0) == 'x') {
+      return "";
+    }
+    return str;
+  default:
+    if (str.charAt(0) == 'x') {
+      if (str.charAt(1) == 'x') {
+        return str.substring(2, str.length());
+      }
+      return str.substring(1, str.length());
+    } else if (str.charAt(1) == 'x') {
+      return str.charAt(0) + str.substring(2, str.length());
+    }
+  }
+  return str;
+}
