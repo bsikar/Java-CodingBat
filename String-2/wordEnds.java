a011732f7a674af4278631d07f1f0e0bdecfbfac
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 23:24:02 -0600

    description

--- String-2/wordEnds.java
+++ String-2/wordEnds.java
@@ -21,6 +21,15 @@
  * SOFTWARE.
  */
 
+/* Given a string and a non-empty word string, return a string made of each char just before and just after
+ * every appearance of the word in the string. Ignore cases where there is no char before or after the word,
+ * and a char may be included twice if it is between two words.
+ *
+ * wordEnds("abcXY123XYijk", "XY") → "c13i"
+ * wordEnds("XY123XY", "XY") → "13"
+ * wordEnds("XY1XY", "XY") → "11"
+ */
+
 public String wordEnds(String str, String word) {
     String z = "";
     for (int i = str.indexOf(word); i != -1; i = str.indexOf(word, ++i)) {
@@ -32,5 +41,6 @@ public String wordEnds(String str, String word) {
         }
         z += str.charAt(i + word.length());
     }
+    
     return z; 
 }
