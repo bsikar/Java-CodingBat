bdce32354f01cda04dde7ff0d4a9dec31eb6a0cf
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 23:17:20 -0600

    description

--- String-2/sameStarChar.java
+++ String-2/sameStarChar.java
@@ -21,6 +21,14 @@
  * SOFTWARE.
  */
 
+/* Returns true if for every '*' (star) in the string, if there are chars both immediately before and after the
+ * star, they are the same.
+ *
+ * sameStarChar("xy*yzz") → true
+ * sameStarChar("xy*zzz") → false
+ * sameStarChar("*xa*az") → true
+ */
+
 public boolean sameStarChar(String str) {
     for (int i = 0; i < str.length() - 2; ++i) {
         if (str.charAt(i+1) == '*' && str.charAt(i) != str.charAt(i+2)) {
