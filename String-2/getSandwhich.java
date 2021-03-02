5b5b3d42e101bec1dd8f5133aa061d200c3a6b72
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 23:16:19 -0600

    description

--- String-2/getSandwhich.java
+++ String-2/getSandwhich.java
@@ -21,6 +21,15 @@
  * SOFTWARE.
  */
 
+/* A sandwich is two pieces of bread with something in between. Return the string that is between the first
+ * and last appearance of "bread" in the given string, or return the empty string "" if there are not two
+ * pieces of bread.
+ *
+ * getSandwich("breadjambread") → "jam"
+ * getSandwich("xxbreadjambreadyy") → "jam"
+ * getSandwich("xxbreadyy") → ""
+ */
+
 public String getSandwich(String str) {
     if (str.length() > 10) {
         return str.substring(str.indexOf("bread") + 5, str.lastIndexOf("bread"));
