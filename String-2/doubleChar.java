6f05f88eaf4bcefe47ba40eaf529edc80cc26f16
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 22:52:25 -0600

    description

--- String-2/doubleChar.java
+++ String-2/doubleChar.java
@@ -21,6 +21,13 @@
  * SOFTWARE.
  */
 
+/* Given a string, return a string where for every char in the original, there are two chars.
+ * 
+ * doubleChar("The") → "TThhee"
+ * doubleChar("AAbb") → "AAAAbbbb"
+ * doubleChar("Hi-There") → "HHii--TThheerree"
+ */
+
 public String doubleChar(String str) {
     String z = "";
     for (int i = 0; i < str.length(); ++i){
