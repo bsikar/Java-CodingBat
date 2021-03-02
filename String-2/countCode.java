382af044dd13615b510406960059912b0c666549
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2021-03-01 23:04:41 -0600

    description

--- String-2/countCode.java
+++ String-2/countCode.java
@@ -21,6 +21,14 @@
  * SOFTWARE.
  */
 
+/* Return the number of times that the string "code" appears anywhere in the given string, except we'll
+ * accept any letter for the 'd', so "cope" and "cooe" count.
+ *
+ * countCode("aaacodebbb") → 1
+ * countCode("codexxcode") → 2
+ * countCode("cozexxcope") → 2
+ */
+
 public int countCode(String str) {
     int sum = 0;
     for (int i = 0; i < str.length() - 3; ++i) {
